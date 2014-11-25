7314dc3e8c65eb9213e008398dd4744a5849376f
Peter Camilleri <peter.c.camilleri@gmail.com>
2014-11-25 17:44:39 -0500

    Added a normalized test.

---
+++ coin_flips_three.rb
@@ -0,0 +1,41 @@
+#A much quicker analysis of possible coin flipping scenarios.
+
+#The analysis class.
+class CoinFlipper
+
+  #Perform an analysis run
+  def run
+    fail "Missing or invalid bit count." unless (1..1024) === (num_flips = ARGV[0].to_i)
+
+    #Build the factorial cache
+    factorial_init(num_flips)
+
+    #Build an array to hold the results
+    results = Array.new(num_flips+1, 0)
+    base = 1 << num_flips
+
+    #Cycle through all possible permutations
+    (0..num_flips).each do |choose|
+      results[choose] = factorial(num_flips) / (factorial(choose)* factorial(num_flips-choose))
+    end
+
+    #Normalize the results
+    normalized = results.collect {|value| Rational(value, base).to_f }
+
+    puts normalized
+  end
+
+  def factorial_init(limit)
+    @factorial = Array.new(limit+1, 1)
+    (2..limit).each { |value| @factorial[value] = value * @factorial[value-1]}
+  end
+
+  def factorial(value)
+    fail "Invalid factorial argument #{value}" unless (0...@factorial.length) === value
+
+    @factorial[value]
+  end
+
+end
+
+CoinFlipper.new.run
